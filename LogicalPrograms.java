6fe341904716ed6b1d5826b1790a22744c6b8201
gaurav459433 <gaurav459433@gmail.com>
2023-02-28 13:55:56 +0530

    Added: Welcome To Logical Programs

---
+++ LogicalPrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignmentsrfp259.day06.logicalprograms;
+
+public class LogicalPrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Logical Programs");
+    }
+}
