3d108baa67cb149bec4733fa4e63153ea6acc29e
gaurav459433 <gaurav459433@gmail.com>
2023-02-28 14:02:04 +0530

    Added: Code For Fibonacci Series

--- LogicalPrograms.java
+++ LogicalPrograms.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.assignmentsrfp259.day06.logicalprograms;
 
+import java.util.Scanner;
+
 public class LogicalPrograms {
     public static void main(String[] args) {
         System.out.println("Welcome To Logical Programs");
+        LogicalPrograms logicalPrograms = new LogicalPrograms();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number: ");
+        int n = sc.nextInt();
+        logicalPrograms.fibonacciSeries(n);
+    }
+
+    // Program For Fibonacci Series
+    public void fibonacciSeries(int n) {
+        int a = 0, b = 1, c = 0;
+        System.out.println(a);
+        System.out.println("1");
+        for (int i = 0; i < n; i++) {
+            a = b + c;
+            System.out.println(a);
+            c = b;
+            b = a;
+        }
     }
 }
